0063e4ac3a2d580d90440869b9e2c89ceae9f0d6
lucalazzaroni <lucalazzaroni95@gmail.com>
2023-12-18 16:23:31 +0100

    classifier training bugfix

--- yolo_adv/classifier/train.py
+++ yolo_adv/classifier/train.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     data_path = '/home/elios/pighetti/adversarial-attacks-pytorch/yolo_adv/classifier/data'
     model = Classifier()
     model.load_dataset(root=data_path, normalize=True, batch_size=16, shuffle=True)
-    model.train(epochs=15, lr=0.001, valid_period=1, ckpt_period=5)
+    model.train(epochs=33, lr=0.001, valid_period=1, ckpt_period=5)
     model.evaluate()
     
